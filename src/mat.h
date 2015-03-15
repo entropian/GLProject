37f8d8cd86185e7c196e511d6bee51038a52e2c1
Bowen Yan <KH2010G@gmail.com>
2015-03-15 04:32:45 -0500

    added a bunch to mat.h and made some name changes in vec.h. didn't test them or compile them.

--- src/mat.h
+++ src/mat.h
@@ -1,6 +1,13 @@
 #include "vec.h"
 
-// TODO: test this
+// Many of the matrix parameters are passed by value to allow the input matrix to also be the output location.
+static const float PI = 3.14159265;
+static const float EPS = 1e-8;
+static const float EPS2 = EPS*EPS;
+static const float EPS3 = EPS*EPS*EPS;
+
+
+// Column major
 typedef union{
     float d_[16];
 
@@ -11,34 +18,204 @@ typedef union{
         float dx, dy, dz, da;
     };
 }mat4;
-
 // TODO: test this
-void transpose(mat4 *output, mat4 *input)
+inline void IDMat(mat4 *output)
 {
-    output->ax = input->ax;
-    output->ay = input->bx;
-    output->az = input->cx;
-    output->aa = input->dx;
-    output->bx = input->ay;
-    output->by = input->by;
-    output->bz = input->cy;
-    output->ba = input->dy;
-    output->cx = input->az;
-    output->cy = input->bz;
-    output->cz = input->cz;
-    output->ca = input->dz;
-    output->dx = input->aa;
-    output->dy = input->ba;
-    output->dz = input->ca;
-    output->da = input->da;
+    int i;
+    for(i = 0; i < 16; i++)
+        output->d_[i] = 0;
+    for(i = 0; i < 4; i++)
+        output->d_[i*4 + i] = 1;
+}
+// TODO: test this
+inline void CopyMat(mat4 *output, const mat4 *input)
+{
+    int i;
+    for(i = 0; i < 16; i++)
+        output->d_[i] = input->d_[i];
+}
+// Re-test this to see if pass by value makes a difference or if pass by reference was fine
+inline void Transpose(mat4 *output, const mat4 input)
+{
+    output->ax = input.ax;
+    output->ay = input.bx;
+    output->az = input.cx;
+    output->aa = input.dx;
+    output->bx = input.ay;
+    output->by = input.by;
+    output->bz = input.cy;
+    output->ba = input.dy;
+    output->cx = input.az;
+    output->cy = input.bz;
+    output->cz = input.cz;
+    output->ca = input.dz;
+    output->dx = input.aa;
+    output->dy = input.ba;
+    output->dz = input.ca;
+    output->da = input.da;
 }
 
-// TODO: test this
-void multScalar(mat4 *output, mat4 *a, float b)
+inline void M4MultScalar(mat4 *output, const mat4 a, const float b)
 {
     int i;
     for(i = 0; i < 16; i++)
     {
-        output->d_[i] = a->d_[i] * b;
+        output->d_[i] = a.d_[i] * b;
     }
+}
+
+// TODO: test this
+inline void M4Mult(mat4 *output, const mat4 a, const mat4 b)
+{
+    int i, j;
+    for(i = 0; i < 4; i++)
+    {
+        vec4 leftRow = {.x = a.d_[i], .y = a.d_[4 + i], .z = a.d_[8 + i], .a = a.d_[12 + i]};
+        for(j = 0; j < 4; j++)
+        {
+            vec4 rightCol = {.x = b.d_[j*4], .y = b.d_[j*4 + 1], .z = b.d_[j*4 + 2], .a = b.d_[j*4 + 3]};
+            output->d_[j*4 + 1] = dotVec4(&leftRow, &rightCol);
+        }
+    }
+}
+
+// TODO: test this
+inline void M4MultVec3(vec3 *output, const mat4 *a, const vec3 b)
+{
+    int i;
+    for(i = 0; i < 3; i++)
+    {
+        vec3 leftRow = {.x = a->d_[i], .y = a->d_[4 + i], .z = a->d_[8 + i]};
+        output->v_[i] = DotV3(&leftRow, b);
+    }
+}
+// TODO: test these two
+inline void M4Add(mat4 *output, const mat4 a, const mat4 b)
+{
+    int i;
+    for(i = 0; i < 16; i++)
+        output->d_[i] = a.d_[i] + b.d_[i];
+}
+
+inline void M4Add(mat4 *output, const mat4 a, const mat4 b)
+{
+    int i;
+    for(i = 0; i < 16; i++)
+        output->d_[i] = a.d_[i] - b.d_[i];
+}
+
+// TODO: test the rotation functions
+inline void MakeXRotation(mat4 *output, const float ang)
+{
+    float cosAng = (float)cos(ang * PI / 180.0);
+    float sinAng = (float)sin(ang * PI / 180.0);
+    
+    int i;
+    for(i = 0; i < 15; i++)
+        output->d_[i] = 0.0;
+    
+    output->d_[0] = 1.0;
+    output->d_[15] = 1.0;
+    output->d_[5] = cosAng;
+    output->d_[6] = -sinAng;
+    output->d_[9] = sinAng;
+    output->d_[10] = cosAng;    
+}
+    
+
+inline void MakeYRotation(mat4 *output, const float ang)
+{
+    float cosAng = (float)cos(ang * PI / 180.0);
+    float sinAng = (float)sin(ang * PI / 180.0);
+    
+    int i;
+    for(i = 1; i < 15; i++)
+        output->d_[i] = 0.0;
+    
+    output->d_[5] = 1.0;
+    output->d_[15] = 1.0;
+    output->d_[0] = cosAng;
+    output->d_[2] = sinAng;
+    output->d_[8] = -sinAng;
+    output->d_[10] = cosAng;    
+}
+
+inline void MakeZRotation(mat4 *output, const float ang)
+{
+    float cosAng = (float)cos(ang * PI / 180.0);
+    float sinAng = (float)sin(ang * PI / 180.0);
+    
+    int i;
+    for(i = 1; i < 15; i++)
+        output->d_[i] = 0.0;
+    
+    output->d_[10] = 1.0;
+    output->d_[15] = 1.0;
+    output->d_[0] = cosAng;
+    output->d_[1] = -sinAng;
+    output->d_[4] = sinAng;
+    output->d_[5] = cosAng;    
+}
+
+// TODO: test this
+inline void MakeScale(mat4 *output, const float s)
+{
+    int i;
+    for(i = 0; i < 15; i++)
+        output->d_[i] = 0.0;
+    
+    output->d_[15] = 1.0;
+    output->d_[0] = s;
+    output->d_[5] = s;
+    output->d_[10] = s;
+}
+// TODO: test this
+inline int IsAffine(const Mat4 *input)
+{
+    if((abs(input->da) + abs(input->ca) + abs(input->ba) + abs(input->aa)) < EPS)
+        return 1;
+    else
+        return 0;
+}
+// TODO: test this
+inline float Norm2M4(const Mat4 *input)
+{
+    float ret;
+    int i;
+    for(i = 0; i < 16; i++)
+        i += input->d_[i]*input->d_[i];
+}
+// TODO: test this
+inline void M4Inv(mat4 *output, const mat4 input)
+{
+     float det = input.d_[0](input.d_[5]*input.d_[10] - input.d_[9]*input.d_[6]) +
+        input.d_[4](input.d_[9]*input.d_[2] - input.d_[1]*input.d_[10]) +
+        input.d_[8](input.d_[1]*input.d_[6] - input.d_[5]*input.d_[2]);
+
+     // assert that |det| > 0?
+     int i;
+     for(i = 0; i < 15; i++)
+         output->d_[i] = 0;
+     // Rotation partition
+     output->ax = (input.by*input.cz - input.cy*input.bz)/det;
+     output->ay = (input.bx*input.cz - input.cx*input.bz)/det;
+     output->az = (input.bx*input.cy - input.cx*input.by)/det;
+     output->bx = (input.ay*input.cz - input.cy*input.az)/det;
+     output->by = (input.ax*input.cz - input.cx*input.az)/det;
+     output->bz = (input.ax*input.cy - input.cx*input.ay)/det;
+     output->cx = (input.ay*input.bz - input.by*input.az)/det;
+     output->cy = (input.ax*input.bz - input.bx*input.az)/det;
+     output->cz = (input.ax*input.by - input.bx*input.ay)/det;
+     // Translation partition
+     output->dx = -(input.dx*output->ax + input.dy*output->bx + input.dz*output->cx);
+     output->dy = -(input.dx*output->ay + input.dy*output->by + input.dz*output->cy);
+     output->dz = -(input.dx*output->az + input.dy*output->bz + input.dz*output->cz);
+
+     output->da = 1.0;
+     // assert
+     mat4 tmp1, tmp2;
+     IDMat(&tmp);
+     M4Mult(&tmp2, input, *output);
+     M4Sub(&tmp, &tmp, &tmp2);
+     assert(IsAffine(output) == 1 && Norm2M4(&tmp) < EPS2);
 }
--- src/vec.h
+++ src/vec.h
@@ -1,7 +1,12 @@
+#ifndef VEC_H
+#define VEC_H
+
 #include <math.h>
+#include <assert.h>
+
 // TODO: add assert equivalents
 typedef union{
-    float v[2];
+    float v_[2];
     struct{
         float x;
         float y;        
@@ -9,7 +14,7 @@ typedef union{
 }vec2;
 
 typedef union{
-    float v[3];
+    float v_[3];
     struct{
         float x;
         float y;
@@ -18,7 +23,7 @@ typedef union{
 }vec3;
 
 typedef union{
-    float v[4];
+    float v_[4];
     struct{
         float x;
         float y;
@@ -29,38 +34,38 @@ typedef union{
 
 // Addition and subtraction
 
-void vec2Add(vec2 *output, vec2 *a, vec2 *b)
+void V2Add(vec2 *output, vec2 *a, vec2 *b)
 {
     output->x = a->x + b->x;
     output->y = a->y + b->y;
 }
 
-void vec2Sub(vec2 *output, vec2 *a, vec2 *b)
+void V2Sub(vec2 *output, vec2 *a, vec2 *b)
 {
     vec2 tmp;
     tmp.x = -b->x;
     tmp.y = -b->y;
-    vec2Add(output, a, &tmp);
+    V2Add(output, a, &tmp);
 }
 
-void vec3Add(vec3 *output, vec3 *a, vec3 *b)
+void V3Add(vec3 *output, vec3 *a, vec3 *b)
 {
     output->x = a->x + b->x;
     output->y = a->y + b->y;
     output->z = a->z + b->z;
 }
 
-void vec3Sub(vec3 *output, vec3 *a, vec3 *b)
+void V3Sub(vec3 *output, vec3 *a, vec3 *b)
 {
     vec3 tmp;
     tmp.x = -b->x;
     tmp.y = -b->y;
     tmp.z = -b->z;
-    vec3Add(output, a, &tmp);
+    V3Add(output, a, &tmp);
 }
 
 // TODO: decide what to do with 4 element vectors
-void vec4Add(vec4 *output, vec4 *a, vec4 *b)
+void V4Add(vec4 *output, vec4 *a, vec4 *b)
 {
     output->x = a->x + b->x;
     output->y = a->y + b->y;
@@ -68,30 +73,30 @@ void vec4Add(vec4 *output, vec4 *a, vec4 *b)
     output->a = a->a + b->a;
 }
 
-void vec4Sub(vec4 *output, vec4 *a, vec4 *b)
+void V4Sub(vec4 *output, vec4 *a, vec4 *b)
 {
     vec4 tmp;
     tmp.x = -b->x;
     tmp.y = -b->y;
     tmp.z = -b->z;
     tmp.a = -b->a;
-    vec4Add(output, a, &tmp);
+    V4Add(output, a, &tmp);
 }
 
-void vec2Mult(vec2 *output, vec2 *a, float b)
+void V2Mult(vec2 *output, vec2 *a, float b)
 {
     output->x = a->x*b;
     output->y = a->y*b;
 }
 
-void vec3Mult(vec3 *output, vec3 *a, float b)
+void V3Mult(vec3 *output, vec3 *a, float b)
 {
     output->x = a->x*b;
     output->y = a->y*b;
     output->z = a->z*b;
 }
 
-void vec4Mult(vec4 *output, vec4 *a, float b)
+void V4Mult(vec4 *output, vec4 *a, float b)
 {
     output->x = a->x*b;
     output->y = a->y*b;
@@ -99,58 +104,58 @@ void vec4Mult(vec4 *output, vec4 *a, float b)
     output->a = a->a*b;
 }
 
-void vec2Div(vec2 *output, vec2 *a, float b)
+void V2Div(vec2 *output, vec2 *a, float b)
 {
-    vec2Mult(output, a, 1/b);
+    V2Mult(output, a, 1/b);
 }
 
-void vec3Div(vec3 *output, vec3 *a, float b)
+void V3Div(vec3 *output, vec3 *a, float b)
 {
-    vec3Mult(output, a, 1/b);
+    V3Mult(output, a, 1/b);
 }
 
-void vec4Div(vec4 *output, vec4 *a, float b)
+void V4Div(vec4 *output, vec4 *a, float b)
 {
-    vec4Mult(output, a, 1/b);
+    V4Mult(output, a, 1/b);
 }
 
-float dotVec2(vec2 *a, vec2 *b)
+float DotV2(vec2 *a, vec2 *b)
 {
     return (a->x*b->x + a->y*b->y);
 }
 
-float dotVec3(vec3 *a, vec3 *b)
+float DotV3(vec3 *a, vec3 *b)
 {
     return (a->x*b->x + a->y*b->y + a->z*b->z);
 }
 
-float dotVec4(vec4 *a, vec4 *b)
+float DotV4(vec4 *a, vec4 *b)
 {
     return (a->x*b->x + a->y*b->y + a->z*b->z + a->a*b->a);
 }
 
-void normalizeVec2(vec2 *output, vec2 *input)
+void NormalizeV2(vec2 *output, vec2 *input)
 {
     float tmp;
     tmp = dotVec2(input, input);
-    vec2Div(output, input, sqrt(tmp));
+    vec2Div(output, input, (float)sqrt(tmp));
 }
 
-void normalizeVec3(vec3 *output, vec3 *input)
+void NormalizeV3(vec3 *output, vec3 *input)
 {
     float tmp;
     tmp = dotVec3(input, input);
-    vec3Div(output, input, sqrt(tmp));
+    vec3Div(output, input, (float)sqrt(tmp));
 }
 
-void normalizeVec4(vec4 *output, vec4 *input)
+void NormalizeV4(vec4 *output, vec4 *input)
 {
     float tmp;
-    tmp = dotVec4(input, input);
-    vec4Div(output, input, sqrt(tmp));
+    tmp = DotV4(input, input);
+    vec4Div(output, input, (float)sqrt(tmp));
 }
 
-void cross(vec3 *output, vec3 *a, vec3 *b)
+void Cross(vec3 *output, vec3 *a, vec3 *b)
 {
     output->x = a->y*b->z - a->z*b->y;
     output->y = a->z*b->x - a->x*b->z;
@@ -158,17 +163,18 @@ void cross(vec3 *output, vec3 *a, vec3 *b)
 }
 
 // TODO: test norm functions
-float normVec2(vec2 *a)
+float NormV2(vec2 *a)
 {
-    return sqrt(dotVec2(a, a));
+    return (float)sqrt(DotV2(a, a));
 }
 
-float normVec3(vec3 *a)
+float NormV3(vec3 *a)
 {
-    return sqrt(dotVec3(a, a));
+    return (float)sqrt(DotV3(a, a));
 }
 
-float normVec4(vec4 *a)
+float NormV4(vec4 *a)
 {
-    return sqrt(dotVec4(a, a));
+    return (float)sqrt(DotV4(a, a));
 }
+
