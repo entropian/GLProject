828378038db21b23b2f1575da373b7fd975709a5
Bowen Yan <KH2010G@gmail.com>
2015-04-16 13:41:07 -0500

    in the process of integrating

--- src/test.cpp
+++ src/test.cpp
@@ -26,9 +26,6 @@ static RigTForm g_view, g_trans;
 static Mat4 g_proj;
 
 GLuint textures[2];
-GLuint shaderProgram1, shaderProgram2;
-GLuint vao1, vao2, vao3;
-GLuint vbo1, vbo2, vbo3, ebo;
 GLint uniTrans1, uniTrans2;
 
 GLFWwindow* window;
@@ -94,7 +91,7 @@ struct Geometry {
     int vboLen = 0, eboLen = 0;
     GLuint shaderProgram;
 
-    Geometry(GLfloat vtx[], unsigned short edx[], int vboLen, int eboLen) {
+    Geometry(GLfloat vtx[], GLuint edx[], int vboLen, int eboLen) {
         this->vboLen = vboLen;
         this->eboLen = eboLen;
 
@@ -160,9 +157,9 @@ struct ShaderState {
     GLint h_aPosition, h_aTexcoord;
     
 
-    ShaderState(const char* vertexSource, const char* fragmentSource) {
+    ShaderState(const char* vs, const char* fs) {
         GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-        glShaderSource(vertexShader, 1, &vertexSource, NULL);
+        glShaderSource(vertexShader, 1, &vs, NULL);
         glCompileShader(vertexShader);
 
         GLint status;
@@ -172,7 +169,7 @@ struct ShaderState {
             fprintf(stderr, "Vertex shader compiled incorrectly.\n");
         
         GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-        glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+        glShaderSource(fragmentShader, 1, &fs, NULL);
         glCompileShader(fragmentShader);
 
         glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
@@ -188,7 +185,7 @@ struct ShaderState {
         glLinkProgram(shaderProgram);
         glDeleteShader(vertexShader);
         glDeleteShader(fragmentShader);
-        //glUseProgram(program);
+        glUseProgram(shaderProgram);
 
         // Retrieve handles to uniform variables
         h_uProj = glGetUniformLocation(shaderProgram, "proj");
@@ -205,7 +202,6 @@ struct ShaderState {
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, textures[0]);
 
-        glUseProgram(shaderProgram);
         int width, height;
         unsigned char* image;
         image = SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
@@ -253,6 +249,7 @@ struct ShaderState {
 
         if(geometry->shaderProgram != shaderProgram)
         {
+            
             glEnableVertexAttribArray(h_aPosition);
             glVertexAttribPointer(h_aPosition, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
             glEnableVertexAttribArray(h_aTexcoord);
@@ -269,6 +266,9 @@ struct ShaderState {
 
 };
 
+Geometry *g_cube, *g_floor, *g_wall;
+ShaderState *flatShader, *texturedShader;
+
 void draw_scene()
 {    
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -284,56 +284,53 @@ void draw_scene()
     //trans = rigTFormToMat(g_trans);
     //trans = transpose(trans)
 
-    glUseProgram(shaderProgram1);
-    glUniformMatrix4fv(uniTrans1, 1, GL_FALSE, &(trans[0]));
-    glUseProgram(shaderProgram2);
-    glUniformMatrix4fv(uniTrans2, 1, GL_FALSE, &(trans[0]));
+    glUseProgram(flatShader->shaderProgram);
+    glUniformMatrix4fv(flatShader->h_uTrans, 1, GL_FALSE, &(trans[0]));
+    glUseProgram(texturedShader->shaderProgram);
+    glUniformMatrix4fv(texturedShader->h_uTrans, 1, GL_FALSE, &(trans[0]));
 
-    glUseProgram(shaderProgram1);
-    glBindVertexArray(vao1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
+
+    glUseProgram(flatShader->shaderProgram);
+    glBindVertexArray(g_cube->vao);
+    glBindBuffer(GL_ARRAY_BUFFER, g_cube->vbo);
+
+    glEnableVertexAttribArray(flatShader->h_aPosition);
+    glVertexAttribPointer(flatShader->h_aPosition, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
+    glEnableVertexAttribArray(flatShader->h_aTexcoord);
+    glVertexAttribPointer(flatShader->h_aTexcoord, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+    
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
     
+    /*
     glUseProgram(shaderProgram2);
     glBindVertexArray(vao2);
     glBindBuffer(GL_ARRAY_BUFFER, vbo2);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    */
 
+
+    /*
     glBindVertexArray(vao3);
     glBindBuffer(GL_ARRAY_BUFFER, vbo3);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    trans = Mat4::makeTranslation(Vec3(-1.0f, 0, 20.0f));
-    trans = transpose(trans);
-    glUniformMatrix4fv(uniTrans2, 1, GL_FALSE, &(trans[0]));
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    trans = Mat4::makeYRotation(90.0f);
-    trans = Mat4::makeTranslation(Vec3(-1.0f, 0, 0)) * Mat4::makeYRotation(90.0f);
-    trans = transpose(trans);
-    glUniformMatrix4fv(uniTrans2, 1, GL_FALSE, &(trans[0]));
-    
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-    trans = Mat4::makeTranslation(Vec3(19.0f, 0, 0)) * Mat4::makeYRotation(90.0f);
-
-    trans = transpose(trans);
-    glUniformMatrix4fv(uniTrans2, 1, GL_FALSE, &(trans[0]));
-
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    */
 
     trans = Mat4::makeTranslation(Vec3(1, 0, 0));
     trans = transpose(trans);
 
+
+    /*
     glUseProgram(shaderProgram1);
     glBindBuffer(GL_ARRAY_BUFFER, vbo1);
     glUniformMatrix4fv(uniTrans1, 1, GL_FALSE, &(trans[0]));
+    */
 
+
+    /*
     glBindVertexArray(vao1);
     glDrawArrays(GL_TRIANGLES, 0, 36);
-
+    */
     glfwSwapBuffers(window);
 }
 
@@ -383,10 +380,10 @@ void cursorPosCallback(GLFWwindow* window, double x, double y)
         g_view = tform * g_view;
         Mat4 view = transpose(rigTFormToMat(g_view));
 
-        glUseProgram(shaderProgram1);
-        glUniformMatrix4fv(uniView1, 1, GL_FALSE, &(view[0]));
-        glUseProgram(shaderProgram2);
-        glUniformMatrix4fv(uniView2, 1, GL_FALSE, &(view[0]));
+        glUseProgram(flatShader->shaderProgram);
+        glUniformMatrix4fv(flatShader->h_uView, 1, GL_FALSE, &(view[0]));
+        glUseProgram(texturedShader->shaderProgram);
+        glUniformMatrix4fv(texturedShader->h_uView, 1, GL_FALSE, &(view[0]));
         //Vec3 trans = g_view.getTranslation();
         //std::cout << "Camera pos: " << trans[0] << " " << trans[1] << " " << trans[2] << "\n";
         draw_scene();
@@ -425,55 +422,13 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     g_view.setTranslation(g_view.getTranslation() - movement);
     Mat4 view = transpose(rigTFormToMat(g_view));
 
-    glUseProgram(shaderProgram1);
-    glUniformMatrix4fv(uniView1, 1, GL_FALSE, &(view[0]));
-    glUseProgram(shaderProgram2);
-    glUniformMatrix4fv(uniView2, 1, GL_FALSE, &(view[0]));
+    glUseProgram(flatShader->shaderProgram);
+    glUniformMatrix4fv(flatShader->h_uView, 1, GL_FALSE, &(view[0]));
+    glUseProgram(texturedShader->shaderProgram);
+    glUniformMatrix4fv(texturedShader->h_uView, 1, GL_FALSE, &(view[0]));
     draw_scene();
 }
 
-void initTextures()
-{
-    // Create 2 textures and load images to them    
-    glGenTextures(2, textures);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textures[0]);
-
-    glUseProgram(shaderProgram2);
-    int width, height;
-    unsigned char* image;
-    image = SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
-    if(image == NULL)
-        fprintf(stderr, "NULL pointer.\n");
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glUniform1i(glGetUniformLocation(shaderProgram2, "texKitten"), 0);
-
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    SOIL_free_image_data(image);
-
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, textures[1]);
-    image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
-    if(image == NULL)
-        fprintf(stderr, "NULL pointer.\n");
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    glUniform1i(glGetUniformLocation(shaderProgram2, "texPuppy"), 1);
-
-    //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    SOIL_free_image_data(image);
-}
 
 int main()
 {
@@ -579,185 +534,50 @@ int main()
         0, 1, 2,
         0, 2, 3
     };
+
+    g_cube = new Geometry(vertices, 36);
+    g_floor = new Geometry(floor_verts, elements, 4, 6);
+    g_wall = new Geometry(wall_verts, elements, 4, 6);
+
+    flatShader = new ShaderState(vertexSource, fragmentSource);
+    texturedShader = new ShaderState(vertexSource, floorFragSrc);
+
+    glUseProgram(flatShader->shaderProgram);
     
-    // Create Vertex Array Object
-    glGenVertexArrays(1, &vao1);
-    glGenVertexArrays(1, &vao2);
-    glGenVertexArrays(1, &vao3);
-    glGenBuffers(1, &vbo1);
-    glGenBuffers(1, &vbo2);
-    glGenBuffers(1, &vbo3);
-    glGenBuffers(1, &ebo);
-
-    
-    glBindVertexArray(vao1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindVertexArray(vao2);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(floor_verts), floor_verts, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
-
-    glBindVertexArray(vao3);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo3);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(wall_verts), wall_verts, GL_STATIC_DRAW);
-    
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
-
-    // Shaders
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexSource, NULL);
-    glCompileShader(vertexShader);
-
-    GLint status;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-
-    if(status != GL_TRUE)
-        fprintf(stderr, "Vertex shader compiled incorrectly.\n");
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-
-    if(status != GL_TRUE)
-        fprintf(stderr, "Fragment shader compiled incorrectly.\n");
-
-    // Link the vertex and fragment shader into a shader program
-    shaderProgram1 = glCreateProgram();
-    glAttachShader(shaderProgram1, vertexShader);
-    glAttachShader(shaderProgram1, fragmentShader);
-    glBindFragDataLocation(shaderProgram1, 0, "outColor");
-    glLinkProgram(shaderProgram1);
-    glUseProgram(shaderProgram1);
-
-
-    // Transform matrices
-    uniTrans1 = glGetUniformLocation(shaderProgram1, "trans");
-
     g_view = RigTForm::lookAt(Vec3(0.0f, 1.0f, 1.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(0, 1, 0));
     Vec3 trans = g_view.getTranslation();
-    std::cout << "Camera pos: " << trans[0] << " " << trans[1] << " " << trans[2] << "\n";
     Mat4 view = rigTFormToMat(g_view);
     view = transpose(view);
 
-
-    uniView1 = glGetUniformLocation(shaderProgram1, "view");
-    glUniformMatrix4fv(uniView1, 1, GL_FALSE, &(view[0]));
+    glUniformMatrix4fv(flatShader->h_uView, 1, GL_FALSE, &(view[0]));
 
     g_proj = Mat4::makeProjection(60.0f, 800.0f/600.0f, 0.1f, 30.0f);
     Mat4 proj = transpose(g_proj);
 
-    GLint uniProj1 = glGetUniformLocation(shaderProgram1, "proj");
-    glUniformMatrix4fv(uniProj1, 1, GL_FALSE, &(proj[0]));
+    glUniformMatrix4fv(flatShader->h_uProj, 1, GL_FALSE, &(proj[0]));
 
-    GLint uniColor1 = glGetUniformLocation(shaderProgram1, "color");
-    glUniform3f(uniColor1, 0.6f, 0.6f, 0.6f);
-
-    glBindVertexArray(vao1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-    // Specify the layout of the vertex data
-    GLint posAttrib1 = glGetAttribLocation(shaderProgram1, "position");
-    glEnableVertexAttribArray(posAttrib1);
-    glVertexAttribPointer(posAttrib1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    GLint texAttrib1 = glGetAttribLocation(shaderProgram1, "texcoord");
-    glEnableVertexAttribArray(texAttrib1);
-    glVertexAttribPointer(texAttrib1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-
-    glBindVertexArray(vao2);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-
-    glEnableVertexAttribArray(posAttrib1);
-    glVertexAttribPointer(posAttrib1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    glEnableVertexAttribArray(texAttrib1);
-    glVertexAttribPointer(texAttrib1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-
-    glBindVertexArray(vao3);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo3);
-
-    glEnableVertexAttribArray(posAttrib1);
-    glVertexAttribPointer(posAttrib1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    glEnableVertexAttribArray(texAttrib1);
-    glVertexAttribPointer(texAttrib1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
+    glUniform3f(flatShader->h_uColor, 0.6f, 0.6f, 0.6f);
 
 
     ////////////////////////////////////////////////////////////////
     // SECOND SHADER
  
-    GLuint floorFragShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(floorFragShader, 1, &floorFragSrc, NULL);
-    glCompileShader(floorFragShader);
-
-    glGetShaderiv(floorFragShader, GL_COMPILE_STATUS, &status);
-
-    if(status != GL_TRUE)
-        fprintf(stderr, "Fragment shader compiled incorrectly.\n");
-
-    // Link the vertex and fragment shader into a shader program
-    shaderProgram2 = glCreateProgram();
-    glAttachShader(shaderProgram2, vertexShader);
-    glAttachShader(shaderProgram2, floorFragShader);
-    glBindFragDataLocation(shaderProgram2, 0, "outColor");
-    glLinkProgram(shaderProgram2);
-    glUseProgram(shaderProgram2);
-
-    // Transform matrices
-    uniTrans2 = glGetUniformLocation(shaderProgram2, "trans");
+    glUseProgram(texturedShader->shaderProgram);
 
     g_view = RigTForm::lookAt(Vec3(0.0f, 1.0f, 1.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(0, 1, 0));
     trans = g_view.getTranslation();
-    std::cout << "Camera pos: " << trans[0] << " " << trans[1] << " " << trans[2] << "\n";
     view = rigTFormToMat(g_view);
     view = transpose(view);
 
-
-    uniView2 = glGetUniformLocation(shaderProgram2, "view");
-    glUniformMatrix4fv(uniView2, 1, GL_FALSE, &(view[0]));
+    glUniformMatrix4fv(texturedShader->h_uView, 1, GL_FALSE, &(view[0]));
 
     g_proj = Mat4::makeProjection(60.0f, 800.0f/600.0f, 0.1f, 30.0f);
     proj = transpose(g_proj);
 
-    GLint uniProj2 = glGetUniformLocation(shaderProgram2, "proj");
-    glUniformMatrix4fv(uniProj2, 1, GL_FALSE, &(proj[0]));
+    glUniformMatrix4fv(texturedShader->h_uProj, 1, GL_FALSE, &(proj[0]));
 
-    GLint uniColor2 = glGetUniformLocation(shaderProgram2, "color");
-    glUniform3f(uniColor2, 0.6f, 0.6f, 0.6f);
+    glUniform3f(texturedShader->h_uColor, 0.6f, 0.6f, 0.6f);
 
-    glBindVertexArray(vao1);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo1);
-    // Specify the layout of the vertex data
-    GLint posAttrib2 = glGetAttribLocation(shaderProgram2, "position");
-    glEnableVertexAttribArray(posAttrib2);
-    glVertexAttribPointer(posAttrib2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    GLint texAttrib2 = glGetAttribLocation(shaderProgram2, "texcoord");
-    glEnableVertexAttribArray(texAttrib2);
-    glVertexAttribPointer(texAttrib2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-
-    glBindVertexArray(vao2);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo2);
-    glEnableVertexAttribArray(posAttrib2);
-    glVertexAttribPointer(posAttrib2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    glEnableVertexAttribArray(texAttrib2);
-    glVertexAttribPointer(texAttrib2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-
-    glBindVertexArray(vao3);
-    glBindBuffer(GL_ARRAY_BUFFER, vbo3);
-    glEnableVertexAttribArray(posAttrib2);
-    glVertexAttribPointer(posAttrib2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    glEnableVertexAttribArray(texAttrib2);
-    glVertexAttribPointer(texAttrib2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-    
-    initTextures();
     // ---------------------------- RENDERING ------------------------------ //
     
     glEnable(GL_DEPTH_TEST);
@@ -774,13 +594,15 @@ int main()
     // ---------------------------- CLEARING ------------------------------ //
 
     // Delete allocated resources
-    glDeleteProgram(shaderProgram1);
-    glDeleteShader(fragmentShader);
-    glDeleteShader(vertexShader);
+    glDeleteProgram(flatShader->shaderProgram);
+    glDeleteProgram(texturedShader->shaderProgram);
     glDeleteTextures(2, textures);
-    glDeleteBuffers(1, &vbo1);
-    glDeleteVertexArrays(1, &vao1);
-    glDeleteVertexArrays(1, &vao2);
+    glDeleteBuffers(1, &(g_cube->vbo));
+    glDeleteBuffers(1, &(g_floor->vbo));
+    glDeleteBuffers(1, &(g_wall->vbo));
+    glDeleteVertexArrays(1, &(g_cube->vao));
+    glDeleteVertexArrays(1, &(g_floor->vao));
+    glDeleteVertexArrays(1, &(g_wall->vao));
 
     // ---------------------------- TERMINATE ----------------------------- //
 
